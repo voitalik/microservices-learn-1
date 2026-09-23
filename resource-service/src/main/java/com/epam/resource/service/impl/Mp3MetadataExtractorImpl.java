19b9466b02f60349aa758ed08e5432a1af2c9059
Roman Voitalik <user@example.com>
2026-09-23 14:11:32 +0300

    reformat Mp3MetadataExtractorImpl

--- resource-service/src/main/java/com/epam/resource/service/impl/Mp3MetadataExtractorImpl.java
+++ resource-service/src/main/java/com/epam/resource/service/impl/Mp3MetadataExtractorImpl.java
@@ -3,6 +3,11 @@ package com.epam.resource.service.impl;
 import com.epam.resource.dto.SongMetadata;
 import com.epam.resource.exception.InvalidResourceException;
 import com.epam.resource.service.Mp3MetadataExtractor;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Locale;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.metadata.TikaCoreProperties;
@@ -12,12 +17,6 @@ import org.apache.tika.parser.mp3.Mp3Parser;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Locale;
 
 @Component
 public class Mp3MetadataExtractorImpl implements Mp3MetadataExtractor {
@@ -41,10 +40,13 @@ public class Mp3MetadataExtractorImpl implements Mp3MetadataExtractor {
             }
             long totalSeconds = seconds.setScale(0, RoundingMode.DOWN).longValueExact();
             return new SongMetadata(null, metadata.get(TikaCoreProperties.TITLE),
-                    metadata.get(XMPDM.ARTIST), metadata.get(XMPDM.ALBUM),
-                    String.format(Locale.ROOT, "%02d:%02d", totalSeconds / 60, totalSeconds % 60),
-                    metadata.get(XMPDM.RELEASE_DATE));
-        } catch (IOException | SAXException | TikaException | ArithmeticException | NumberFormatException exception) {
+                                    metadata.get(XMPDM.ARTIST), metadata.get(XMPDM.ALBUM),
+                                    String.format(Locale.ROOT, "%02d:%02d",
+                                                  totalSeconds / 60,
+                                                  totalSeconds % 60),
+                                    metadata.get(XMPDM.RELEASE_DATE));
+        } catch (IOException | SAXException | TikaException | ArithmeticException |
+                 NumberFormatException exception) {
             throw invalidMp3();
         }
     }
