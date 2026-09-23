8a611a6d27c928aebb2394e25eb08e354b2c66fc
Roman Voitalik <user@example.com>
2026-09-23 16:29:37 +0300

    fix: reworks error messages for SongDto fields

--- song-service/src/main/java/com/epam/songservice/dto/SongDto.java
+++ song-service/src/main/java/com/epam/songservice/dto/SongDto.java
@@ -1,6 +1,5 @@
 package com.epam.songservice.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -8,16 +7,27 @@ import jakarta.validation.constraints.Size;
 
 public record SongDto(
         @NotNull(message = "ID is required")
-        @Positive(message = "ID must be a positive integer") Integer id,
-        @NotBlank(message = "Name is required")
-        @Size(max = 100, message = "Name must be between 1 and 100 characters") String name,
-        @NotBlank(message = "Artist is required")
-        @Size(max = 100, message = "Artist must be between 1 and 100 characters") String artist,
-        @NotBlank(message = "Album is required")
-        @Size(max = 100, message = "Album must be between 1 and 100 characters") String album,
+        @Positive(message = "ID must be a positive integer")
+        Integer id,
+
+        @NotNull(message = "Song name is required")
+        @Size(min = 1, max = 100, message = "Song name must be between 1 and 100 characters")
+        String name,
+
+        @NotNull(message = "Artist name is required")
+        @Size(min = 1, max = 100, message = "Artist name must be between 1 and 100 characters")
+        String artist,
+
+        @NotNull(message = "Album name is required")
+        @Size(min = 1, max = 100, message = "Album name must be between 1 and 100 characters")
+        String album,
+
         @NotNull(message = "Duration is required")
         @Pattern(regexp = "[0-9]{2}:[0-5][0-9]",
-                message = "Duration must be in mm:ss format with leading zeros") String duration,
+                message = "Duration must be in mm:ss format with leading zeros")
+        String duration,
+
         @NotNull(message = "Year is required")
-        @Pattern(regexp = "(19|20)[0-9]{2}", message = "Year must be between 1900 and 2099") String year) {
+        @Pattern(regexp = "(19|20)[0-9]{2}", message = "Year must be between 1900 and 2099")
+        String year) {
 }
