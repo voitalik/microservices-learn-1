f9c8c7f894d9393a5860805b1b3b67dbe369cebc
Roman Voitalik <user@example.com>
2026-09-23 16:39:58 +0300

    fix: reworks error message for existing song metadata

--- song-service/src/main/java/com/epam/songservice/exception/SongAlreadyExistsException.java
+++ song-service/src/main/java/com/epam/songservice/exception/SongAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.epam.songservice.exception;
 
 public class SongAlreadyExistsException extends RuntimeException {
     public SongAlreadyExistsException(Integer id) {
-        super("Song metadata with ID=" + id + " already exists");
+        super("Metadata for resource ID=" + id + " already exists");
     }
 }
